3755cb4c2360b3b5fb462278db68dad0fe39110c
yonoelia5 <101924705+yonoelia5@users.noreply.github.com>
2022-06-23 18:22:12 +0200

    Implementación de métodos de TipoUsuarioServiceImpl.java

--- src/main/java/com/escuela/service/TipoUsuarioServiceImpl.java
+++ src/main/java/com/escuela/service/TipoUsuarioServiceImpl.java
@@ -1,36 +1,76 @@
 package com.escuela.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.escuela.entity.TipoUsuario;
+import com.escuela.repository.TipoUsuarioRepository;
 
+
+/**
+ * The Class TipoUsuarioServiceImpl.
+ */
 @Service
 public class TipoUsuarioServiceImpl implements TipoUsuarioService {
 
+	/** The tipo usuario repository. */
+	@Autowired
+	private TipoUsuarioRepository tipoUsuarioRepository;
+
+	/**
+	 * Gets the all tipo usuario.
+	 *
+	 * @return the all tipo usuario
+	 */
 	@Override
 	public List<TipoUsuario> getAllTipoUsuario() {
-		// TODO Auto-generated method stub
-		return null;
+		return tipoUsuarioRepository.findAll();
 	}
 
+	/**
+	 * Save tipo usuario.
+	 *
+	 * @param tipoUsuario the tipo usuario
+	 */
 	@Override
 	public void saveTipoUsuario(TipoUsuario tipoUsuario) {
-		// TODO Auto-generated method stub
-		
+		this.tipoUsuarioRepository.save(tipoUsuario);
 	}
 
+	/**
+	 * Gets the tipo usuario by id.
+	 *
+	 * @param id the id
+	 * @return the tipo usuario by id
+	 */
 	@Override
 	public TipoUsuario getTipoUsuarioById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		final Optional<TipoUsuario> optional = this.tipoUsuarioRepository.findById(id);
+		TipoUsuario tipoUsuario = null;
+
+		if(optional.isPresent()) {
+			tipoUsuario = optional.get();
+
+		}else {
+			throw new RuntimeException("Tipo de usuario no encontrado por id :: " + id);
+		}
+
+		return tipoUsuario;
 	}
 
+	/**
+	 * Delete tipo usuario by id.
+	 *
+	 * @param id the id
+	 */
 	@Override
 	public void deleteTipoUsuarioById(long id) {
-		// TODO Auto-generated method stub
-		
+		this.tipoUsuarioRepository.deleteById(id);
+
 	}
 
 }
