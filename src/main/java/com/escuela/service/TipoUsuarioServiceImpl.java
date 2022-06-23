314a9b6f8f275537fbfe1a5ea8df440f28f5d734
Francisco <franciscoalanzavesromero@gmail.com>
2022-06-23 17:56:02 +0200

    Métodos de impl Tipo Usuario Service

--- src/main/java/com/escuela/service/TipoUsuarioServiceImpl.java
+++ src/main/java/com/escuela/service/TipoUsuarioServiceImpl.java
@@ -1,5 +1,36 @@
 package com.escuela.service;
 
-public class TipoUsuarioServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.escuela.entity.TipoUsuario;
+
+@Service
+public class TipoUsuarioServiceImpl implements TipoUsuarioService {
+
+	@Override
+	public List<TipoUsuario> getAllTipoUsuario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveTipoUsuario(TipoUsuario tipoUsuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TipoUsuario getTipoUsuarioById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteTipoUsuarioById(long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
